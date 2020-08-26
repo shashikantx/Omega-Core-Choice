e7bdaa1f0c574c7a4b6b7019c2fd4840771d412e
vridhi <vridhi@samiteon.com>
2020-08-26 16:36:00 +0530

    vendor fetch criteria

---
+++ dist/app/dto/tx/fetch-vendor-criteria-dto.d.ts
@@ -0,0 +1,11 @@
+import { AddressVo } from "@omega/core";
+export interface FetchVendorCriteriaDto {
+    orgId: string;
+    brId: string;
+    branchId: string;
+    startDate: Date;
+    endDate: Date;
+    address: AddressVo;
+    timeStartDate: Date;
+    timeEndDate: Date;
+}
